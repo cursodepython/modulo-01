18465e7ee4457e5ababcecb1c61095296fd6032b
Francisco Calaça Xavier <chicocx@gmail.com>
2017-05-12 08:52:47 -0300

    atualizacoes

--- aula-00.py
+++
@@ -1,8 +0,0 @@
-"""
-Programa inicial
-para apresentacao
-do ambiente python
-"""
-texto = input("Digite um texto:") #obtem um valor digitado pelo usuario
-print("Texto digitado: " + texto) #imprime o valor digitado pelo usuario no console
-
