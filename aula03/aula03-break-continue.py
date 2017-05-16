ea1da64fde2504c32b340f0b2dc84998f7d3e628
Francisco Calaça Xavier <chicocx@gmail.com>
2017-05-16 09:53:30 -0300

    aula 03

--- aula03/aula03-break-continue.py
+++ aula03/aula03-break-continue.py
@@ -1,4 +1,12 @@
+print("Exemplo continue")
 for v in range(1, 10):
     if(v == 8):
         continue
     print("Valor: " + str(v))
+
+print("===============")
+print("Exemplo break")    
+for v in range(1, 10):
+    if(v == 8):
+        break
+    print("Valor: " + str(v))    
